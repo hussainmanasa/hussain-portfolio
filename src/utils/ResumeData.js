8503c1a3247eec237ea4deb8237a1ab5c3998cb0
Hussain Manasawala <hussain.manasawala@liquidtelecom.co.za>
2024-02-03 10:21:01 +0530

    updated packages and resume

--- src/utils/ResumeData.js
+++ src/utils/ResumeData.js
@@ -10,15 +10,16 @@ import typescriptIcon from "../assets/images/skills/typescript.svg";
 import javascriptIcon from "../assets/images/skills/javascript.svg";
 import githubIcon from "../assets/images/skills/github.svg";
 import magentoIcon from "../assets/images/skills/magento.svg";
+import nextIcon from "../assets/images/skills/nextjs.svg";
 
-export default {
+const ResumeData = {
   name: "Hussain Manasawala",
   title: "Web/Mobile App Developer",
   birthday: "22nd July 1997",
   email: "manasawala_hussain@yahoo.com",
   job: "Tata Consultancy Services",
   phone: "+91 9920973313",
-  about_title: "Hi, I’m Hussain. Nice to meet you!",
+  about_title: "Hi, I am Hussain. Nice to meet you!",
   about:
     "You can also call me an Innovative Frontend Developer. Someone who loves to build scalable and maintainable Web and Mobile Apps. I don't define myself by the work I've done rather I define myself by the work I want to do. Skills can be taught, personality is inherent. Learning is one the strengths which I prefer to continue throughout and keep challenging myself. \n\n Being the youth am fueled by high energy levels and boundless enthusiasm. I'm easily inspired and willing to follow my fascinations wherever they take me. I'm a very multi-talented, expressive, passionate with natural ability to inspire and motivate. \n\n Curently working with Noesoft as a UI developer and in my spare time you'll find me doing photography or buffering some latest episodes on OTT platforms.",
   socials: {
@@ -49,16 +50,16 @@ export default {
   },
   experiences: [
     {
+      title: "Senior Software Engineer - Neosoft Technologies",
+      date: "June 2022 - Present",
+      description:
+        "Innovative frontend developer full of life and inspiration. Working with my hands to make magic on the internet. Moreover, I am also involded in mentoring the new borns in the IT world.",
+    },
+    {
       title: "System Engineer - Tata Consultancy Services",
       date: "August 2019 - June 2022",
       description:
         "Working as an innovative front end developer creating scalable web and mobile apps using plane HTML CSS and Javascript, using frameworks like Angular and React and Adobe Magento CMS.",
-    },
-    {
-      title: "Software Engineer - Neosoft Technologies",
-      date: "June 2022 - Present",
-      description:
-        "Innovative frontend developer full of life and inspiration. Working with my hands to make magic on the internet. Moreover, I am also involded in mentoring the new borns in the IT world.",
     },
   ],
   education: [
@@ -73,24 +74,12 @@ export default {
   ],
   skils: [
     {
-      title: "Angular",
-      icon: angularIcon,
-    },
-    {
-      title: "Ionic",
-      icon: ionicIcon,
-    },
-    {
       title: "React",
       icon: reactIcon,
     },
     {
-      title: "Bootstrap",
-      icon: bootstrapIcon,
-    },
-    {
-      title: "Typescript",
-      icon: typescriptIcon,
+      title: "Next Js",
+      icon: nextIcon,
     },
     {
       title: "Javascript",
@@ -101,8 +90,26 @@ export default {
       icon: githubIcon,
     },
     {
+      title: "Angular",
+      icon: angularIcon,
+    },
+    {
+      title: "Ionic",
+      icon: ionicIcon,
+    },
+    {
+      title: "Bootstrap",
+      icon: bootstrapIcon,
+    },
+    {
+      title: "Typescript",
+      icon: typescriptIcon,
+    },
+    {
       title: "Magento",
       icon: magentoIcon,
     },
   ],
 };
+
+export default ResumeData;
